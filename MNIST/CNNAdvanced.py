fff75a1a829bb170eadda16d0fd11a984f2d9367
virajbhalala <virajbhalala@gmail.com>
2018-04-25 11:08:48 -0400

    code rebase

--- MNIST/CNNAdvanced.py
+++ MNIST/CNNAdvanced.py
@@ -4,6 +4,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import numpy as np
 
+
 # Import data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
